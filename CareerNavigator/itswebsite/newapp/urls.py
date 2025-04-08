3c9eb5c61ca0edfdc72fb2016fc8f97d5ecb01c2
sonkamesh <kobernik.sofia@yandex.ru>
2025-04-08 21:25:43 +0700

    Delete CareerNavigator directory

--- CareerNavigator/itswebsite/newapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.newapp_home, name='newapp_home'),
-    path('<int:pk>', views.NewappDetailView.as_view(), name='newapp-detail'),
-]
